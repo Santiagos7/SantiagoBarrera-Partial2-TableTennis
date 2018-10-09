6adab4effe930ba2601372a4140e32fd1fd5f11a
santiagos7 <santiagodsbg@gmail.com>
2018-10-09 12:31:49 -0500

    Parcial2

---
+++ SantiagoBarrera-TableTennis/Assets/Scripts/LeftPaddle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftPaddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Si se presiona W, se movera hacia arriba.
+		if (Input.GetKey(KeyCode.W))
+		{
+			this.transform.Translate (Vector3.up);
+
+		}
+
+
+		//Si se presiona S, se movera hacia abajo.
+		if (Input.GetKey(KeyCode.S))
+		{
+			this.transform.Translate (Vector3.down);
+
+		}
+
+		//Si se presiona A, se movera hacia izquierda.
+		if (Input.GetKey(KeyCode.A))
+		{
+			this.transform.Translate (Vector3.left);
+
+		}
+
+		//Si se presiona D, se movera hacia derecha.
+		if (Input.GetKey(KeyCode.D))
+		{
+			this.transform.Translate (Vector3.right);
+
+		}
+		
+	}
+}
---
+++ SantiagoBarrera-TableTennis/Assets/Scripts/RightPaddle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightPaddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Si se presiona UpArrow, se movera hacia arriba.
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			this.transform.Translate (Vector3.up);
+
+		}
+
+
+		//Si se presiona DownArrow, se movera hacia abajo.
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			this.transform.Translate (Vector3.down);
+
+		}
+
+		//Si se presiona LeftArrow, se movera hacia izquierda.
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			this.transform.Translate (Vector3.left);
+
+		}
+
+		//Si se presiona RightArrow, se movera hacia derecha.
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			this.transform.Translate (Vector3.right);
+
+		}
+		
+	}
+}
